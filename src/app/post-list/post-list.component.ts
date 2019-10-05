377a71ed09ebf00898a6e78281435fc2c43fb0fd
Carles Rius <realbluehead@gmail.com>
2019-10-05 18:32:19 +0200

    - Post creation / show / edit / delete handles and modals POC

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
+import PostDTO from '../models/PostDTO';
 
 @Component({
   selector: 'app-post-list',
@@ -8,12 +9,68 @@ import { PostService } from '../post.service';
 })
 export class PostListComponent implements OnInit {
   posts = [];
+  deleteModalOpen = false;
+  showModalOpen = false;
+  editModalOpen = false;
+  newModalOpen = false;
+  currentPost = null;
   constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.retrievePosts();
+  }
+
+  retrievePosts() {
     this.postService.list().subscribe((data: any) => {
       this.posts = data;
     });
   }
 
+  onDelete(post: PostDTO) {
+    this.currentPost = post;
+    this.deleteModalOpen  = true;
+  }
+  cancelPostDeletion() {
+    this.deleteModalOpen = false;
+  }
+
+  doPostDeletion() {
+    this.deleteModalOpen = false;
+    /*this.postService.remove(this.currentPost).subscribe((data: any) =>{
+      this.retrievePosts();
+    });*/
+  }
+
+  hideShowPostModal() {
+    this.showModalOpen = false;
+  }
+
+  onEdit(post: PostDTO) {
+    this.currentPost = post;
+    this.editModalOpen = true;
+  }
+
+  cancelPostEdition() {
+    this.editModalOpen = false;
+  }
+  doPostEdition() {
+    this.editModalOpen = false;
+   /* this.postService.update(this.currentPost).subscribe((data:any) => {
+      this.retrievePosts();
+    });*/
+  }
+
+  onNew() {
+    this.newModalOpen = true;
+  }
+  cancelPostCreation() {
+    this.newModalOpen = false;
+  }
+
+  doPostCreation() {
+    this.newModalOpen = false;
+    /* this.postService.create(this.currentPost).subscribe((data:any) => {
+      this.retrievePosts();
+    });*/
+  }
 }
